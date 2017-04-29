d8cbf4db44526353853dfb5fe22062dbb5bc32f9
turingbot <tabaghi.m@gmail.com>
2017-04-29 22:25:41 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = ""
+bot_token = "312712967:AAGdxA71K3jwBwPoLYPjsqJ3rBJymaK7Avw"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 212400472
+sudo_id = 151499613
